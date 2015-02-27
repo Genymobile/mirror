d920cba5e8d41fd48d7fe0b254dd3c11f81eb7d3
fnoel <fnoel@genymobile.com>
2015-02-27 23:52:38 +0100

    Add dependencies towards lib

--- sample/src/main/java/com/genymobile/mirror/Sample.java
+++ sample/src/main/java/com/genymobile/mirror/Sample.java
@@ -2,6 +2,8 @@ package com.genymobile.mirror;
 
 public class Sample {
     public static void main(String[] args) {
-        System.out.println("Hello bitch!");
+        System.out.println("Hello Mirror!");
+
+        Mirror mirror = new Mirror();
     }
 }
