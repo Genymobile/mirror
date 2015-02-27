b5b91659f315c3443317cdf80d1b0f5b9ff8ebae
Sylvain Galand <sgaland@genymobile.com>
2015-02-28 00:08:02 +0100

    Add Field and Class annotations in lib

---
+++ lib/src/main/java/com/genymobile/mirror/Class.java
@@ -0,0 +1,13 @@
+package com.genymobile.mirror;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface Class {
+    String value();
+}
