9ccf4e3815b1b9e6e7e17a76d3b8ea1b444eef8b
Sylvain Galand <sgaland@genymobile.com>
2015-02-28 12:24:06 +0100

    Add a MirrorDeveloperException

---
+++ lib/src/main/java/com/genymobile/mirror/exception/MirrorDeveloperException.java
@@ -0,0 +1,9 @@
+package com.genymobile.mirror.exception;
+
+public class MirrorDeveloperException extends RuntimeException {
+
+    public MirrorDeveloperException(String message) {
+        super(message);
+    }
+
+}
